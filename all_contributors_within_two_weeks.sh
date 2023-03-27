da684a897a210cfa04043aa7f2888aa66cc8260f
Carson Yang <yangchuansheng33@gmail.com>
2023-03-27 11:30:16 +0800

    Update all_contributors_within_two_weeks.sh

--- all_contributors_within_two_weeks.sh
+++ all_contributors_within_two_weeks.sh
@@ -5,13 +5,13 @@
 FROM_DATE=$(date -d'14 day ago' +%Y-%m-%d)
 UNTIL_DATE=$(date +%Y-%m-%d)
 
-for i in $(cat ks_repo.txt);
+for i in $(cat repo.txt);
 do
   for j in {1..30};
   do
-      curl -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$i/commits?since=${FROM_DATE}T00:00:00Z&until=${UNTIL_DATE}T00:00:00Z&per_page=100&page=$j"|jq -r '.[].committer.login'|sort|uniq >> ks_all_contributors_within_two_weeks_source.txt
-      curl -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$i/commits?since=${FROM_DATE}T00:00:00Z&until=${UNTIL_DATE}T00:00:00Z&per_page=100&page=$j"|jq -r '.[].author.login'|sort|uniq >> ks_all_contributors_within_two_weeks_source.txt
+      curl -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$i/commits?since=${FROM_DATE}T00:00:00Z&until=${UNTIL_DATE}T00:00:00Z&per_page=100&page=$j"|jq -r '.[].committer.login'|sort|uniq >> all_contributors_within_two_weeks_source.txt
+      curl -s -H "Accept: application/vnd.github.v3+json" -H "Authorization: token $GITHUB_TOKEN" "https://api.github.com/repos/$i/commits?since=${FROM_DATE}T00:00:00Z&until=${UNTIL_DATE}T00:00:00Z&per_page=100&page=$j"|jq -r '.[].author.login'|sort|uniq >> all_contributors_within_two_weeks_source.txt
   done
 done
 
-cat ks_all_contributors_within_two_weeks_source.txt|grep -Ev "\-bot|\[bot\]|^null$"|sort|uniq > ks_all_contributors_within_two_weeks_${DATE}.txt
+cat all_contributors_within_two_weeks_source.txt|grep -Ev "\-bot|\[bot\]|^null$"|sort|uniq > all_contributors_within_two_weeks_${DATE}.txt
